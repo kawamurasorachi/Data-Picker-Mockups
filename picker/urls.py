427fb76b55788a864ffec0bbf34a9b89d942a395
kawamurasorachi <sorakawa0218@gmail.com>
2021-03-28 10:12:30 +0900

    made blocking invalid user traffic system

--- picker/urls.py
+++ picker/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import TopView, HomeView
 
-app_name='picker'
-
+app_name = 'picker'
 urlpatterns = [
-    path('', IndexView.as_view(), name='top'),
+    path('', TopView.as_view(), name='top'),
+    path('<slug:username>/', HomeView.as_view(), name='home'),
 ]
--- picker/views.py
+++ picker/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView 
+from django.shortcuts import render, redirect, reverse
+from django.views.generic import TemplateView, DetailView
+from .models import Topic
+from django.conf import settings
+from django.shortcuts import get_object_or_404, HttpResponseRedirect
+from user.models import CustomUser
+from .scripts import crawler
+from django.contrib.auth.mixins import UserPassesTestMixin
 
-# Create your views here.
 
-class IndexView(TemplateView):
-    template_name = "index.html"
+class OnlyYouMixin(UserPassesTestMixin):
+    raise_exception = True
+
+    def test_func(self):
+        user = self.request.user
+        return user.username == self.kwargs['username'] or user.is_superuser
+
+class TopView(TemplateView):
+    template_name = "top.html"
+
+class HomeView(OnlyYouMixin, DetailView):
+    template_name = "home.html"
+    slug_field = "username"
+    slug_url_kwarg = "username"
+    model = CustomUser
+
+    def get_object(self):
+        object = get_object_or_404(CustomUser, username=self.kwargs.get("username"))
+        if self.request.user.username == object.username:
+            return object
+        else:
+            return HttpResponseRedirect(reverse('picker:top'))
+
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context["topics_list"] = Topic.objects.filter(user=user)
+        return context
+    
